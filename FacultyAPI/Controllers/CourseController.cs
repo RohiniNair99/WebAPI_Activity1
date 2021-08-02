c1c41aef2284280ce538381ff07979c078de8d2d
Rohini Nair <rohiniacchu@gmail.com>
2021-08-02 19:30:30 +0530

    Pushed activity

---
+++ FacultyAPI/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FacultyAPI.Models;
+
+namespace FacultyAPI.Controllers
+{
+    public class CourseController : ApiController
+    {
+        // GET: api/Course
+        public List<string> Get()
+        {
+            List<Cours> courseList = new List<Cours>();
+            List<string> coursePrint = new List<string>();
+            using (ConnectFaculty connect = new ConnectFaculty())
+            {
+                courseList = connect.Courses.ToList();
+
+            }
+            foreach (Cours course in courseList)
+            {
+                coursePrint.Add($"{course.CourseId}  {course.CourseTitle}  {course.CourseDuration}  {course.CourseMode} ");
+            }
+            return coursePrint;
+        }
+
+    }
+}
---
+++ FacultyAPI/Controllers/FacultyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FacultyAPI.Models;
+
+namespace FacultyAPI.Controllers
+{
+    public class FacultyController : ApiController
+    {
+        // GET: api/Faculty
+        public List<string> Get()
+        {
+            List<Faculty> facultyList = new List<Faculty>();
+            List<string> facultyPrint = new List<string>();
+            using (ConnectFaculty connect = new ConnectFaculty())
+            {
+                facultyList = connect.Faculties.ToList();
+                
+            }
+            foreach(Faculty faculty in facultyList )
+            {
+                facultyPrint.Add($"{faculty.PSNO}  {faculty.FacultyName}  {faculty.EmailId}");
+            }
+            return facultyPrint;
+        }
+
+       
+    }
+}
